7dc2cdaf13af0934d3208dc99f5c235520073a47
nsiderwolverine <justin.joseph0@yahoo.com>
2016-08-05 01:05:44 +0000

    Add methods to Stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,24 @@
 class Stock < ActiveRecord::Base
+  def self.find_by_ticker(ticker)
+    where(ticker: ticker).first
+  end
+  
+  def self.new_from_lookup(ticker)
+    stock = StockQuote::Stock.quote(ticker)
+    return nil unless stock.name
+    
+    new_stock = new(ticker: stock.symbol, name: stock.name)
+    new_stock.last_price = new_stock.price
+    new_stock
+  end
+  
+  def price
+    closing_price = StockQuote::Stock.quote(ticker).close
+    return "#{closing_price} (Closing)" if closing_price
+    
+    opening_price = StockQuote::Stock.quote(ticker).open
+    return "#{opening_price} (Opening)" if opening_price
+    
+    "Unavailable"
+  end
 end
