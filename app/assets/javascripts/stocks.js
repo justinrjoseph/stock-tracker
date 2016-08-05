e5f057a172d9765efb583c0bd2e759ec60fbec40
nsiderwolverine <justin.joseph0@yahoo.com>
2016-08-05 20:54:41 +0000

    Add before and after spinner for UI when stock form submitted

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -7,13 +7,18 @@ $(function() {
 var lookupStock ;
 
 lookupStock = function() {
- $('#stock-lookup-form').on('ajax:success', function(event, data, status) {
+  $('#stock-lookup-form').on('ajax:before', function(event, data, status) {
+    showSpinner();
+  }).on('ajax:after', function(event, data, status) {
+    hideSpinner();
+  });
+  
+  $('#stock-lookup-form').on('ajax:success', function(event, data, status) {
     $('#stock-lookup').replaceWith(data);
     lookupStock();
- }).on('ajax:error', function(event, data, status) {
-   $('#stock-lookup-results').replaceWith(' ');
-   $('#stock-lookup-errors').replaceWith('<div class="col-md-12 alert alert-danger">Stock was not found.</div>');
- });
-  
-  
+  }).on('ajax:error', function(event, data, status) {
+    hideSpinner();
+    $('#stock-lookup-results').replaceWith(' ');
+    $('#stock-lookup-errors').replaceWith('<div class="col-md-12 alert alert-danger">Stock was not found.</div>');
+  });
 }
