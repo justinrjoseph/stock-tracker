2d56d0f59a11d7a77647a1d8b83a2d4d4062a810
nsiderwolverine <justin.joseph0@yahoo.com>
2016-08-06 02:57:59 +0000

    Remove turbolinks; add user show template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
 //= require_tree .
+
 var hideSpinner = function() {
     $('#spinner').hide();
 }
